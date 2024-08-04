a505f633c5a04af83b7f4fa3e20b60c469f6e82b
Adin <adinramadhan10@gmmail.com>
2024-08-04 18:43:14 +0700

    feat: user and initialize db and laravel

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+Route::get('/', function () {
+    return view('pages.auth.auth-login');
+});
+
+Route::middleware(['auth'])->group(function (){
+    Route::get('home', function (){
+        return view('pages.dashboard',['type_menu'  => 'home']);
+    })->name('name');
+
+    Route::resource('users', UserController::class);
+});
